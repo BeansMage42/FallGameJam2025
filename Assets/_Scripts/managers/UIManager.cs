7daf3b58fe2b0e95f86c5da5df7ff2a60e972776
Ethan22-dev <ethanam46@gmail.com>
2025-10-25 21:38:09 -0400

    Timer UI added
    Countdown timer created at the top of the screen

--- Assets/_Scripts/managers/UIManager.cs
+++ Assets/_Scripts/managers/UIManager.cs
@@ -1,8 +1,11 @@
+using TMPro;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
     public static UIManager Instance;
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float remainingTime;
 
     //Singleton (carried between scenes)
     private void Awake() //Acts before Start()
@@ -21,6 +24,21 @@ public class UIManager : MonoBehaviour
         }
     }
 
+    private void Timer()
+    {
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }
+        else if (remainingTime < 0)
+        {
+            remainingTime = 0;
+        }
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
     void Start() //Acts before Update()
     {
         
@@ -28,6 +46,6 @@ public class UIManager : MonoBehaviour
 
     void Update()
     {
-        
+        Timer();
     }
 }
